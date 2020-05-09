46ac56facee2291b9bbddf9d5f0d2e0cd2671e78
akshat-crypto <akshatsoni396@gmail.com>
2020-05-09 14:14:53 +0530

    #1cmmnt

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-print 'HELLO WORLD. WELCOME TO MY FIRST PAGE OF TASK1';
-print 'this line is the first chng in the developer branch';
-print 'this line is the second chng in the developer branch';
+print 'HELLO WORLD. WELCOME TO MY FIRST PAGE OF TASK1. this line is the first chng in the developer branch. this line is the second chng in the developer branch';
 
 ?>
